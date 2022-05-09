7d5e8936c8ba93a9c6c9a33476e9167a4df6c877
chuhoon <khchu1220@naver.com>
2022-05-09 17:14:09 +0900

    2205091714

--- FE28/007.js
+++ FE28/007.js
@@ -10,3 +10,16 @@ result = '';
 for (let i = 0; i < txt.length; i++) {
   result = txt[i] + result;
 }
+
+for (let i = 2; i < 10; i++) {
+  for (let j = 1; j < 10; j++) {
+    console.log(`${i} X ${j} = ${i * j}`);
+  }
+}
+
+let i = 0;
+let sum = 0;
+while (i < 101) {
+  sum += i;
+  i += 2;
+}
