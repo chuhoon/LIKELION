fb74d1ef3c2ee317e13e13241208a31a0f84b948
chuhoon <khchu1220@naver.com>
2022-06-30 23:05:13 +0900

    webpack

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -1,3 +1,9 @@
-import { plus } from './plus.js';
+import { plus } from './plus.js'
+import './styles.css';
+import tiger from './tiger.png';
 
-console.log(plus(2, 5));
+// console.log(plus(2, 4));
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `<img src="${tiger}">`;
+})
--- webpack/src/plus.js
+++ webpack/src/plus.js
@@ -1,15 +1,14 @@
 // const obj = {};
 
-// 모듈 패턴 전역패턴을 오염시키지 않기 위해서
-// plus 함수에 접근하려면 obj를 거쳐야한다.
 // (() => {
-//   function plus(a, b) {
-//     return a + b;
-//   }
+//     function plus(a, b) {
+//         return a + b;
+//     }
 
-//   obj.plus = plus;
+//     obj.plus = plus;
 // })();
 
+
 export function plus(a, b) {
-  return a + b;
+    return a + b;
 }
